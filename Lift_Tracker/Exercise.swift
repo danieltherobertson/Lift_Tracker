11a1718b9498a87bdb6f4f160770af6eba4571f1
Daniel Robertson <danieltherobertson@gmail.com>
2017-05-06 00:50:59 +0100

    Adds basic models, initial main menu wireframe

---
+++ Lift_Tracker/Exercise.swift
@@ -0,0 +1,20 @@
+//
+//  Exercise.swift
+//  Lift_Tracker
+//
+//  Created by Daniel Robertson on 06/05/2017.
+//  Copyright © 2017 Daniel Robertson. All rights reserved.
+//
+
+import Foundation
+
+struct Exercise {
+    var name: String!
+    var sets: Int!
+    var reps: Int!
+    var restTime: Int!
+    var weight: Double!
+    var targetWeight: Double!
+}
+
+
---
+++ Lift_Tracker/Workout.swift
@@ -0,0 +1,15 @@
+//
+//  Workout.swift
+//  Lift_Tracker
+//
+//  Created by Daniel Robertson on 06/05/2017.
+//  Copyright © 2017 Daniel Robertson. All rights reserved.
+//
+
+import Foundation
+
+struct  Routine {
+    var name: String!
+    var exercises: [Exercise]!
+    var performancesPerWeek: Int
+}
